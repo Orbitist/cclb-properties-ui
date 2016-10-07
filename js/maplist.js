2b73b5772ca95e05ba2a64ef10dbe6d24a52d71d
Nicholas Gunner <gunnernr@gmail.com>
2016-10-07 10:36:54 -0400

    added so and started styling the list

--- js/maplist.js
+++ js/maplist.js
@@ -3,10 +3,19 @@ function renderMapList() {
   // Create the map list items
   for (var i = 0; i < orbitistPointsGeojsonCleaned.features.length; i++) {
     var feature = orbitistPointsGeojsonCleaned.features[i];
+    var propertyImage = '<img src="' + feature.properties.image + '" />';
+    if (feature.properties.image == null){
+      propertyImage = '';
+    }
     if (numberedPoints == "true"){
-      var  listItem  = '<hr><div class="map-list-item" id="' + feature.properties.point_id + '"><img src="' + feature.properties.point_marker_url + '" /><p>' + feature.properties.point_position_number + " - " + feature.properties.point_title + '</p></div><script>document.getElementById("' + feature.properties.point_id + '").addEventListener("click", function () {map.flyTo({center:[' + feature.geometry.coordinates + '],zoom: 18,bearing: 90 * (.5 - Math.random()),pitch: 60});});</script>';
+      var  listItem  = '<hr><div class="map-list-item" id="' + 'property' + i + '"><img src="' + feature.properties.image + '" /><p>' + (i +1) + " - " + feature.properties.point_title + '</p></div><script>document.getElementById("' + 'property' + i + '").addEventListener("click", function () {map.flyTo({center:[' + feature.geometry.coordinates + '],zoom: 18,bearing: 90 * (.5 - Math.random()),pitch: 60});});</script>';
     } else {
-      var  listItem  = '<hr><div class="map-list-item" id="' + feature.properties.point_id + '"><img src="' + feature.properties.point_marker_url + '" /><p>' + feature.properties.point_title + '</p></div><script>document.getElementById("' + feature.properties.point_id + '").addEventListener("click", function () {map.flyTo({center:[' + feature.geometry.coordinates + '],zoom: 18,bearing: 90 * (.5 - Math.random()),pitch: 60});});</script>';
+      var  listItem  = '<hr><div class="map-list-item" id="' + 'property' + i + '">' + propertyImage +
+      '<h5>' + feature.properties.name + '</h5>' +
+      '<p><strong>Feature: </strong> Test feature</p>' +
+      '<p><strong>Feature: </strong> Test feature</p>' +
+      '<p><strong>Feature: </strong> Test feature</p>' +
+      '</div><script>document.getElementById("' + 'property' + i + '").addEventListener("click", function () {map.flyTo({center:[' + feature.geometry.coordinates + '],zoom: 18,bearing: 90 * (.5 - Math.random()),pitch: 60});});</script>';
     }
     $('div.map-list').append(listItem);
   }
